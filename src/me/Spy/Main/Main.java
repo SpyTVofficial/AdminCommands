fedddbbfa754956f186f2d2d89aa5021ac9e24db
itsSpyTV <imorlux@icloud.com>
2021-04-26 09:16:53 +0200

    Initialized Main Class

---
+++ src/me/Spy/Main/Main.java
@@ -0,0 +1,19 @@
+package me.Spy.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		init();
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	private void init() {
+		
+	}
+
+}
